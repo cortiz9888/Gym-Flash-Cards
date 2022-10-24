a6cd497f3eb9734ac6e8435decf219fe84a0e009
cortiz9888 <114939185+cortiz9888@users.noreply.github.com>
2022-10-24 15:48:10 -0500

    Chris Ortiz | Gym-Flash-Cards
    worked on html and css files. tomorrow will be functionality and working on making them match and flip

---
+++ Gym-Flash-Cards.js
@@ -0,0 +1,31 @@
+//lets get em to flip or slowly reveal on click
+//shuffle
+//highlight when theyre correct(green)
+//highlight when theyre wrong (red)
+//randomize
+//get them seperate or together
+//
+//
+//
+const cardOne = "card1"
+const cardTwo = "card2"
+const cardThree = "card3"
+const cardFour = "card4"
+const cardFive = "card5"
+const cardSix = "card6"
+const cardSeven = "card7"
+const cardEight = "card8"
+const cardNine = "card9"
+const cardTen = "card10"
+const cardEleven = "card11"
+const cardTwelve = "card12"
+const cardThirteen = "card13"
+const cardFourteen = "card14"
+const cardFifteen = "card15"
+const cardSixteen = "card16"
+const cardSeventeen = "card17"
+const cardEighteen = "card18"
+const cardNineteen = "card19"
+const cardTwenty = "card20"
+const cardTwentyone = "card21"
+const cardTwentytwo = "card22"
